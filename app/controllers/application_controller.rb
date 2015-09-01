556aaa1cc71427a3e3e649d0af974e0ff2ab4dcd
gabrielcorado <gabrielcorado@mail.com>
2015-08-31 22:03:23 -0300

    Adding Trailblazer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # Trailblazer
+  include Trailblazer::Operation::Controller
+  require 'trailblazer/operation/controller/active_record'
+  include Trailblazer::Operation::Controller::ActiveRecord # named instance variables.
 end
